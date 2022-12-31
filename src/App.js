ee718ef048067e22a434f46ba87a6fb69b7889cb
spyatmycode <akejunifemi11@gmail.com>
2022-12-31 14:41:31 +0100

    Project Init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+
+
+function App() {
+  return (
+    <div className="App">
+      Hello
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from './App'
+
+const Index = () => {
+  return <App />
+}
+
+const rootElement = document.getElementById("root")
+
+const root = ReactDOM.createRoot(rootElement)
+
+root.render(<Index />)
+
+
