ac098fa7df0d91628fa6e545c73357e3fde17630
JamilAmasa <jamilamasa@gmail.com>
2023-01-01 19:50:29 +0100

    form update

--- src/Pages/SignInPage.js
+++ src/Pages/SignInPage.js
@@ -5,14 +5,17 @@ export default function SignInPage() {
   return (
     <div className={SignInCss.forms}>
         <div className={SignInCss.card}>
+        <i className={`${SignInCss.icon} fa-solid fa-user`}></i>
+        <h2 className={SignInCss.mheading}>Welcome!</h2>
+        <p className={SignInCss.mtext}>Sign in to your account</p>
             <form>
                 <div className={SignInCss.formGroup}>
                     <label>E-mail</label>
-                    <input type="text"/>
+                    <input type="text" placeholder='Enter your email'/>
                 </div>
                 <div className={SignInCss.formGroup}>
                     <label>Password</label>
-                    <input type="password"/>
+                    <input type="password" placeholder='Enter your password'/>
                 </div>
                 <div className={SignInCss.formGroup}>
                     <button className={SignInCss.btn} type='submit'>Sign In</button>
