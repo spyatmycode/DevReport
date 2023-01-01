7bb7e7b5f802b83d17a762b5cdb4a99e120ae552
JamilAmasa <jamilamasa@gmail.com>
2023-01-01 21:31:18 +0100

    minor UI bug fixes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React from 'react';
-
-
 import Article from './components/Article';
-import SignUpPage from './Pages/SignUpPage';
-
 
 
 function App() {
   return (
     <div>
       {/* <Article /> */}
-      <SignUpPage />
     </div>
     
   );
